7c7a7a3c3c247c318576bd1ca5cd302d5a08d06d
Sperrotta10 <santiagoperrotta10@gmail.com>
2026-03-20 12:59:10 -0400

    feat: reestructurar la aplicación para incluir rutas y componentes de página; actualizar estilos CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from './providers/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="lol-mind-theme">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
